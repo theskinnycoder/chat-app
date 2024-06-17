ba6bc0227b245a3990e42e994f6883d4ac466d75
theskinnycoder <18131a0505@gvpce.ac.in>
2024-06-17 13:38:31 +0530

    chore: Update label and toaster components

--- README.md
+++ README.md
@@ -19,9 +19,24 @@ pnpm build
 pnpm lint
 ```
 
-## Tech Stack :
+## Tech Stack
 
 - React.js and Radix UI for the UI
-- Tailwind CSS for styling
+- Tailwind CSS & CSS variables for styling
 - Framer Motion for animations
 - Zustand for state management
+
+## Features
+
+- [x] Send messages
+- [x] Create new chats
+- [x] Delete chats
+- [x] Emoji picker
+- [x] Auto scroll to bottom
+- [x] Keyboard Navigatable
+- [x] ARIA support
+- [x] Animations
+- [ ] Dark Mode support
+- [ ] Pin Chats to top
+- [x] Chat search
+- [ ] Create chat group (I added the base configuration in accordance to this, can be extended very easily)
--- src/components/chat/dm/chat-sidebar/index.tsx
+++ src/components/chat/dm/chat-sidebar/index.tsx
@@ -1,6 +1,8 @@
 import { useChatStore } from '@/store'
 import ChatSidebarButton from './chat-sidebar-button'
 import ChatSidebarHeader from './chat-sidebar-header'
+import { Input } from '@/components/ui/input'
+import { useState } from 'react'
 
 interface SidebarProps {
 	isCollapsed: boolean
@@ -10,6 +12,14 @@ interface SidebarProps {
 export default function ChatSidebar({ isCollapsed }: SidebarProps) {
 	const { chats } = useChatStore()
 
+	const [searchText, setSearchText] = useState('')
+
+	const filteredChats = chats.filter(
+		chat =>
+			chat.type === 'dm' &&
+			chat.toUser.name.toLowerCase().includes(searchText.toLowerCase()),
+	)
+
 	return (
 		<div
 			data-collapsed={isCollapsed}
@@ -18,9 +28,19 @@ export default function ChatSidebar({ isCollapsed }: SidebarProps) {
 			{/* Chat Sidebar Header */}
 			{!isCollapsed && <ChatSidebarHeader />}
 
+			{/* Search */}
+			{!isCollapsed && (
+				<Input
+					placeholder='Search chats'
+					value={searchText}
+					onChange={e => setSearchText(e.target.value)}
+					className='max-w-xs'
+				/>
+			)}
+
 			{/* Chat Sidebar List Buttons */}
 			<nav className='grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2'>
-				{chats.map(chat => (
+				{filteredChats.map(chat => (
 					<ChatSidebarButton
 						key={chat.id}
 						isCollapsed={isCollapsed}
